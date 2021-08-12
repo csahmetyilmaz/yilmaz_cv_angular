0a8ba6ab3db9a3bd852daec8ecc03ea6206b898f
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2021-08-12 10:36:11 +0200

    Create cv service, some dispatcher functions add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/cv.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {Education} from "../models/education";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+import {Observable} from "rxjs";
+import {Experience} from "../models/experience";
+import {Skill} from "../models/skill";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CvService {
+
+  siteUrl = environment.siteUrl;
+  jsonUrl =  "/assets/json/"
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getEducation(): Observable<Education[]> {
+    return this.http.get<Education[]>(this.jsonUrl + "education.json")
+  }
+
+  public getExperience(): Observable<Experience[]> {
+    return this.http.get<Experience[]>(this.jsonUrl + "experiences.json")
+  }
+
+  public getSkills():Observable<Skill[]> {
+    return this.http.get<Skill[]>(this.jsonUrl + "skills.json")
+  }
+}
